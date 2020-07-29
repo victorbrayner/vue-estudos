99ef0318651903f3466808a68840ddf392c73a4b
Victor Brayner <victorbrayner@gmail.com>
2020-07-29 18:41:18 -0300

    finalizado projeto-02

---
+++ curso-cod3r/projeto-02/src/components/NewTask.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="new-task">
+      <input v-model="name"
+      @keydown.enter="add"
+      type="text"
+      class="form-element"
+      placeholder="O que vamos fazer hoje?">
+      <button @click="add" class="form-element" >+</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: ''
+        }
+    },
+    methods: {
+        add(){
+            this.$emit('taskAdded', { name: this.name })
+            this.name = ''
+        }
+    },
+}
+</script>
+
+<style>
+    .new-task{
+        margin: 35px;
+    }
+
+    .form-element{
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid lightgray;
+        padding: 5px 10px 8px;
+        color: white;
+    }
+
+    input.form-element{
+        width: 500px;
+        background: lightslategray;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+
+    button.form-element {
+        border-left: none;
+        background-color: mediumaquamarine;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
+</style>
---
+++ curso-cod3r/projeto-02/src/components/Task.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="task"
+    @click="$emit('taskStateChanged', task)"
+    :class="stateClass">
+    <span @click.stop="$emit('taskDeleted',task)" class="close">x</span>
+      <p>{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        task: { type: Object, required: true }
+    },
+    computed: {
+        stateClass(){
+            return {
+                pending: this.task.pending,
+                done: !this.task.pending
+            }
+        }
+    },
+}
+</script>
+
+<style>
+    .task{
+        position: relative;
+        box-sizing: border-box;
+        width: 200px;
+        height: 100px;
+        padding: 10px;
+        border-radius: 8px;
+        font-size: 1.2rem;
+        font-weight: 300;
+        cursor: pointer;
+        user-select: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .pending{
+        border-left: 12px solid darkred;
+        background-color: red;
+    }
+
+    .done {
+        color: darkgrey;
+        border-left: 12px solid darkgreen;
+        background-color: green;
+        text-decoration: line-through;
+    }   
+
+    .pending .close{
+        background-color: darkred;
+    }
+
+    .done .close{
+        background-color: darkgreen
+    }
+
+    .close{
+        position: absolute;
+        right: 10px;
+        top: 10px;
+        font-size: 0.9rem;
+        height: 20px;
+        width: 20px;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+    }
+</style>
