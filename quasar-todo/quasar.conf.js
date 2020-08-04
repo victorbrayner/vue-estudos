d1f955a92401a7bc9ac996fa22e3c8229ff4d0d7
Victor Brayner <victorbrayner@gmail.com>
2020-08-04 16:25:35 -0300

    função addTasks finalizada

--- quasar-todo/quasar.conf.js
+++ quasar-todo/quasar.conf.js
@@ -98,7 +98,7 @@ cfg.module.rules.push({
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Dialog', 'Notify']
     },
 
     // animations: 'all', // --- includes all animations
--- quasar-todo/src/pages/Todo.vue
+++ quasar-todo/src/pages/Todo.vue
@@ -1,20 +1,60 @@
 <template>
   <q-page class="bg-grey-3 column">
+
+    <div class="row q-pa-sm bg-primary">
+      <q-input 
+        class="col"
+        @keyup.enter="addTask"
+        square
+        filled
+        bg-color="white"
+        v-model="newTask"
+        placeholder="Adicionar Tarefa"
+        dense>
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" @click="addTask" />
+        </template>
+      </q-input>
+    </div>
+
     <q-list class="bg-white" separator bordered>
       <q-item
-        v-for="task in tasks"
+        v-for="(task, index) in tasks"
         :key="task.title"
         @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1': task.done }"
         clickable
         v-ripple>
         <q-item-section avatar>
-          <q-checkbox v-model="task.done" color="primary" class="no-pointer-events" />
+          <q-checkbox
+            v-model="task.done"
+            color="primary"
+            class="no-pointer-events" />
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
         </q-item-section>
+        <q-item-section
+          v-if="task.done"
+          side>
+          <q-btn flat round color="primary" icon="delete"
+            dense @click.stop="deleteTask(index)" />
+        </q-item-section>
       </q-item>
     </q-list>
+
+    <div 
+      v-if="!tasks.length"
+      class="no-tasks absolute-center">
+      <q-icon
+        name="check"
+        size="100px"
+        color="primary" />
+      <div class="text-h5 text-primary text-center">
+        Sem tarefas
+      </div>
+    </div>
+
   </q-page>
 </template>
 
@@ -22,21 +62,42 @@
 export default {
   data() {
     return {
-      tasks:[
-        {
-          title: 'Task One',
-          done: false
-        },
-        {
-          title: 'Task Two',
-          done: false
-        },
-        {
-          title: 'Task Three',
-          done: false
-        }
-      ]
+      newTask:'',
+      tasks:[]
     }
+  },
+  methods: {
+        deleteTask(index){
+          this.$q.dialog({
+            title: 'Confirmar',
+            message: 'Você deseja deletar a tarefa?',
+            cancel: true,
+            persistent: true
+          }).onOk(() => {
+            this.tasks.splice(index, 1)
+            this.$q.notify('Tarefa Deletada')
+          })
+        },
+        addTask(event){
+          this.tasks.push({
+            title: this.newTask,
+            done: false
+            })
+          this.newTask = ''
+        }
   },
 }
 </script>
+
+<style lang="scss">
+  .done {
+    .q-item__label{
+      text-decoration: line-through;
+      color: #bbbbbb;
+    }
+  }
+
+  .no-tasks{
+    opacity: 0.5;
+  }
+</style>
